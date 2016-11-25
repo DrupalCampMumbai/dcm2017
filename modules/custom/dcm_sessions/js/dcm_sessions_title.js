df01a7294795ad3e569e5d272a359e83611e6325
Vinay15 <vinay.abhang@blisstering.com>
2016-11-25 13:33:24 +0530

    Modified sessions title as per requirement.

--- modules/custom/dcm_sessions/js/dcm_sessions_title.js
+++ modules/custom/dcm_sessions/js/dcm_sessions_title.js
@@ -19,47 +19,62 @@
         attach: function (context, settings) {
 
             // Sessions title JS.
-            console.log('hi');
-            $('.view-display-id-sessions_listing .view-content h3').each(function( index ) {
-               // console.log($(this));
-            $(this).addClass('dcm-special-title');
-            var session_title_text = $(this).text();
-            var session_title_trimmed = session_title_text.trim();
-            //console.log(session_title_trimmed);
-            var split_session_title = session_title_trimmed.split(' ');
-            //console.log(split_session_title);
+            // Execute for each title only once.
+            $('.view-display-id-sessions_listing .view-content h3').once().each(function( index ) {
 
-            for (var i = 0; i <= split_session_title.length; i++) {
-                if (i != 0 && split_session_title[i - 1] != "" && (split_session_title[i + 1] == "" || i == (split_session_title.length - 1))) {
-                    //$(this).html('<span>'+split_session_title[i]+'</span>');
-                    var text = $(this).text();
-                    var split_session_text = text.split(' ');
-                    console
-                    for (var i = 0; i <= split_session_text.length; i++) {
-                        if (i != 0 && split_session_text[i - 1] != "" && (split_session_text[i + 1] == "" || i == (split_session_text.length - 1))) {
+                // Add custom class to h3 element.
+                $(this).addClass('dcm-special-title');
 
-                            var split_session_trim = text.replace(split_session_text[i], '');
-                            console.log(split_session_text[i]);
-                            console.log(split_session_trim);
+                // Fetch title text.
+                var session_title_text = $(this).text();
 
+                // Trim whitespaces.
+                var session_title_trimmed = session_title_text.trim();
+
+                // Split trimmed text with whitespaces.
+                var split_session_title = session_title_trimmed.split(' ');
+
+                // For each element in split_session_title array.
+                for (var i = 0; i <= split_session_title.length; i++) {
+
+                    // Fetch h3 element's html.
+                    var h3_html = $(this).html();
+
+                    // Check previous and next element, if previous element has text and next element is empty, then
+                    // fetch that element.
+                    if (i != 0 && split_session_title[i - 1] != "" && (split_session_title[i + 1] == "" || i == (split_session_title.length - 1))) {
+
+                        // Fetch h3 element's text.
+                        var text = $(this).text();
+
+                        // Split text based on whitespaces.
+                        var split_session_text = text.split(' ');
+
+                        // Remove text from html.
+                        $(this).html(h3_html.replace(session_title_trimmed, ' '));
+
+                        // Fetch new html.
+                        var replacement_html = $(this).html();
+
+                        for (var i = 0; i <= split_session_text.length; i++) {
+                            if (i != 0 && split_session_text[i - 1] != "" && (split_session_text[i + 1] == "" || i == (split_session_text.length - 1))) {
+
+                                var split_session_trim = text.replace(split_session_text[i], '');
+
+                                // Remove multiple text.
+                                var new_replacement_html = replacement_html.replace(session_title_trimmed, ' ');
+
+                                // Add span tag for last element.
+                                $(this).html(new_replacement_html + split_session_trim + '<span class="session-title-span">' + split_session_text[i] + '</span>');
+
+                                // Add alt tag for image.
+                                $(this).find('img').attr('alt', session_title_trimmed);
+
+                            }
                         }
                     }
                 }
-            }
             });
-
-            // Array.prototype.clean = function(deleteValue) {
-            //     for (var i = 0; i < this.length; i++) {
-            //         if (this[i] == deleteValue) {
-            //             this.splice(i, 1);
-            //             i--;
-            //         }
-            //     }
-            //     return this;
-            // };
-            //
-            // var split_session_title_array = split_session_title.clean("");
-            // console.log(split_session_title_array);
         }
     };
 
